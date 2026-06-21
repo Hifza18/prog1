93503c29c763f5a21ea31a02cd3ac000e8d8f400
Hifza <hifzasanuberbs@gmail.com>
2026-06-21 19:03:01 +0530

    first commit

---
+++ prog6/src/main/java/sanuber_devops/prog6/App.java
@@ -0,0 +1,13 @@
+package sanuber_devops.prog6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
